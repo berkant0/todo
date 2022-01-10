f020873c485f6603b53055c217995268c88cca1b
Berkant Dagtekin <dagtekinberkant03@gmail.com>
2022-01-10 21:30:49 +0300

    Model file completed

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Berkant on 10.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                //todo add list view
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+        }
+    }
+}
